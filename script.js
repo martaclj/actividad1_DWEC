467e45b42476c6a72585cb25d05bc41596de893f
martaclj <104948192+martaclj@users.noreply.github.com>
2024-10-18 21:04:16 +0200

    scriptjs first try

---
+++ script.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var productos = [];
+
+  function mostrarProducto(producto) {
+    let tr = document.createElement('tr');
+    let td1 = document.createElement('td');
+    let td2 = document.createElement('td');
+    td1.textContent = producto.title;
+    td2.textContent = producto.price;
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    let tabla = document.getElementById('tablaProductos');
+    tabla.appendChild(tr);
+  }
+
+fetch('https://jsonblob.com/api/1296797574781329408')
+  .then(response => response.json())
+  .then(infoArray => {
+    productos = infoArray.products;
+    console.log(productos);
+    productos.forEach(producto => {
+      mostrarProducto(producto.title);
+    }).catch((err) => {
+      console.log(err);
+    });
+  });
+
+});
